4500b6fbf60fb40de30c16f4d1cb37408947966b
lextm <lextm@localhost>
2010-12-11 14:24:32 +0800

    Updated for SharpDevelop 4.0 builds.

--- Src/AStyleStyle.cs
+++ Src/AStyleStyle.cs
@@ -1,51 +1,50 @@
 // lextm: this is AStyle style enum.
 // Copyright (C) 2006  Lex Mark
-// 
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-namespace Lextm.AStyle {
+namespace Lextm.AStyle
+{
+    ///<summary>
+    ///AStyle styles.
+    /// </summary>
+    /// <remarks>AStyle has many build-in styles. It can be extended by command line.</remarks>
+    public enum AStyleStyle {
 
-
-	///<summary>
-	///AStyle styles.
-	/// </summary>
-	/// <remarks>AStyle has many build-in styles. It can be extended by command line.</remarks>
-	public enum AStyleStyle {
-
-		/// <summary>
-		///User-defined command line.
-		/// </summary>
-		CommandLine = 1,
-		/// <summary>
-		///GNU style.
-		/// </summary>
-		Gnu,
-		/// <summary>
-		///ANSI style.
-		/// </summary>
-		Ansi,
-		/// <summary>
-		///Java style.
-		/// </summary>
-		Java,
-		/// <summary>
-		///Linux style.
-		/// </summary>
-		Linux,
-		/// <summary>
-		/// Kernighan-Ritchie style.
-		/// </summary>
-		KernighanRitchie
-	};
+        /// <summary>
+        ///User-defined command line.
+        /// </summary>
+        CommandLine = 1,
+        /// <summary>
+        ///GNU style.
+        /// </summary>
+        Gnu,
+        /// <summary>
+        ///ANSI style.
+        /// </summary>
+        Ansi,
+        /// <summary>
+        ///Java style.
+        /// </summary>
+        Java,
+        /// <summary>
+        ///Linux style.
+        /// </summary>
+        Linux,
+        /// <summary>
+        /// Kernighan-Ritchie style.
+        /// </summary>
+        KernighanRitchie
+    };
 }
--- Src/OptionsProxy.cs
+++ Src/OptionsProxy.cs
@@ -3,7 +3,7 @@
  * User: Administrator
  * Date: 2006-10-13
  * Time: 20:47
- * 
+ *
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 
@@ -13,43 +13,45 @@ using System.Reflection;
 
 namespace Lextm.AStyle
 {
-	/// <summary>
-	/// Description of OptionsProxy.
-	/// </summary>
-	public class OptionsProxy
-	{
-		public OptionsProxy()
-		{
-		}
-		
-		private readonly static string fileName =
-			Path.Combine(Path.GetDirectoryName(
-				Assembly.GetExecutingAssembly().Location),
-				"settings.xml");
-		
-		public static void SaveOptions() {
-			PreferencesManager.SavePreferences(fileName, options);
-		}
-		
-		private static Preferences options;
-		
-		public static Preferences Options {
-			get {
-				if (options == null) {
+    /// <summary>
+    /// Description of OptionsProxy.
+    /// </summary>
+    public class OptionsProxy
+    {
+        public OptionsProxy()
+        {
+        }
 
-					options = (Preferences)PreferencesManager.LoadPreferences(fileName,
-					                                             typeof(Preferences));
-					if (options == null) {
-						options = new Preferences();
-						options.AStyleParams = Preferences.DefaultAStyleParams;
-						options.AStyleConfiguration = new AStyleSettings();
-						options.AStyleConfiguration.ConvertTabs = true;
-						options.AStyleConfiguration.IndentNamespace = true;
-						options.AStyleConfiguration.Style = AStyleStyle.KernighanRitchie;
-					}
-				}
-				return options;
-			}
-		}
-	}
+        private readonly static string fileName =
+            Path.Combine(Path.GetDirectoryName(
+                             Assembly.GetExecutingAssembly().Location),
+                         "settings.xml");
+
+        public static void SaveOptions()
+        {
+            PreferencesManager.SavePreferences(fileName, options);
+        }
+
+        private static Preferences options;
+
+        public static Preferences Options
+        {
+            get {
+                if (options == null) {
+
+                    options = (Preferences)PreferencesManager.LoadPreferences(fileName,
+                              typeof(Preferences));
+                    if (options == null) {
+                        options = new Preferences();
+                        options.AStyleParams = Preferences.DefaultAStyleParams;
+                        options.AStyleConfiguration = new AStyleSettings();
+                        options.AStyleConfiguration.ConvertTabs = true;
+                        options.AStyleConfiguration.IndentNamespace = true;
+                        options.AStyleConfiguration.Style = AStyleStyle.KernighanRitchie;
+                    }
+                }
+                return options;
+            }
+        }
+    }
 }
